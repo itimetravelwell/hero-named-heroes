419c074198575d6424b1090d9c2265e1ea921b4e
itimetravelwell <charles.jackson@georgebrown.ca>
2015-10-16 14:55:21 -0400

    Last Change before End of Class

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function(e) {
-	$("img[alt='Batman']").addClass('shown');
+	// $("img[alt='Batman']").addClass('shown');
+	$('img').first().addClass('shown');
 	
 	var name = $('img').attr('title');
+	// var image = $()
 	
 	$('h1').text(name);
 	
-	$("button[class='next']").click(function(){
- 		$("img[alt='Batman']").removeClass('shown');
+	$("button[class='next']").click(function(){ 
+ 		$('.shown').next().addClass('shown');
+ 		$('.shown').prev().removeClass('shown');
 	});
 
 
 });
+
+// Can you make the initial load better or more effiencent?
+// set the variables from the go and grab the urls?
+
+// Button listener?
+
+// Fade in and fade out properties.
 
 
 
